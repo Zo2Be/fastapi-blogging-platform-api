f6a423d21dd8694081ca7ca6f2689eda8e605d7e
Elizaveta <elizaveta.yaremova@yandex.ru>
2025-02-06 16:25:04 +0300

    Add naming conventions for models

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,15 @@
 from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
 
+
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
+
 
 class DatabaseConfig(BaseModel):
     url: PostgresDsn
@@ -14,6 +17,15 @@ class DatabaseConfig(BaseModel):
     echo_pool: bool = False
     max_overflow: int = 50
     pool_size: int = 10
+
+    naming_conventions: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -25,5 +37,6 @@ class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
+
 
 settings = Settings()
