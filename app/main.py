6f525af5c160b85e2d96a7e38a35492811b11c37
Elizaveta <elizaveta.yaremova@yandex.ru>
2025-02-12 10:49:51 +0300

    Use orjson

--- app/main.py
+++ app/main.py
@@ -16,10 +16,18 @@ async def lifespan(app: FastAPI):
     # shutdown
     print("Dispose engine")
     await db_helper.dispose()
+
+
 main_app = FastAPI(
+    default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
-main_app.include_router(api_router, prefix=settings.api.prefix,)
+main_app.include_router(
+    api_router,
+    prefix=settings.api.prefix,
+)
 
 if __name__ == "__main__":
-    uvicorn.run("main:main_app", host=settings.run.host, port=settings.run.port, reload=True)
+    uvicorn.run(
+        "main:main_app", host=settings.run.host, port=settings.run.port, reload=True
+    )
