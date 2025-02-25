9d5028501270f26fd9a7773f73c8bcf5496b8738
Elizaveta <elizaveta.yaremova@yandex.ru>
2025-02-25 14:06:58 +0300

    Add relationship in user for post

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -3,9 +3,13 @@ __all__ = [
     "Base",
     "User",
     "AccessToken",
+    "Post",
+    "Category",
 ]
 
 from .db_helper import db_helper
 from .base import Base
 from .user import User
 from .access_token import AccessToken
+from .post import Post
+from .category import Category
--- app/core/models/user.py
+++ app/core/models/user.py
@@ -4,6 +4,7 @@ from fastapi_users_db_sqlalchemy import (
     SQLAlchemyBaseUserTable,
     SQLAlchemyUserDatabase,
 )
+from sqlalchemy.orm import Mapped, relationship
 
 from core.types.user_id import UserIdType
 from .base import Base
@@ -11,9 +12,12 @@ from .mixins.int_id_pk import IntIdPKMixin
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
+    from .post import Post
 
 
 class User(Base, IntIdPKMixin, SQLAlchemyBaseUserTable[UserIdType]):
+    posts: Mapped[list["Post"]] = relationship(back_populates="user")
+
     @classmethod
     def get_db(cls, session: "AsyncSession"):
         return SQLAlchemyUserDatabase(session, cls)
