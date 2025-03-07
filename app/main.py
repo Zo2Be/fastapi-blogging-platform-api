9cd149d378f3f419bfcd28826dcf1b162e189ef4
Elizaveta <elizaveta.yaremova@yandex.ru>
2025-03-07 12:21:06 +0300

    Use redis client

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
 from api import router as api_router
+from core.cache import redis_client
 from core.config import settings
 from core.models import db_helper
 
@@ -12,8 +13,10 @@ from core.models import db_helper
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup
+    await redis_client.ping()
     yield
     # shutdown
+    await redis_client.close()
     await db_helper.dispose()
 
 
